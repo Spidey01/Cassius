17c950ecd356655673e8a951f380d776639bcc4f
Terry Matthew Poulin <BigBoss1964@gmail.com>
2010-11-10 15:19:14 +0000

    Added enums for supported languages and backends
    Currently, that is still a work in progress for Lua.

---
+++ Include/Cassius.hpp
@@ -0,0 +1,26 @@
+#ifndef CASSIUS_INCLUDE_CASSIUS_HPP
+#define CASSIUS_INCLUDE_CASSIUS_HPP
+
+namespace Cassius {
+
+    /**
+     * Enumeration of what scripting languages may be used
+     */
+    enum ScriptLanguages {
+        LANG_LUA,
+    };
+
+    /**
+     * Enumeration of backends that may be embedded.
+     *
+     * This will eventually be used, to handle situations such as choosing
+     * whether Mozilla SpiderMonkey or Google V8 is used as a backend for
+     * EcmaScript/JavaScript.
+     */
+    enum Backends {
+        IMPL_DEFAULT,
+        IMPL_LUA,
+    };
+
+}
+#endif
