32b9368be563bc8bdedd6e2ab6aca72d31feaf6c
Terry M. Poulin <BigBoss1964@gmail.com>
2021-12-27 16:22:43 -0500

    Remove premake4 files.
    premake4 is out, premake5 is in, and I'm aiming for CMake instead.

--- Source/premake4.lua
+++
@@ -1,9 +0,0 @@
-
-include "Cassius"
-include "CluaEngine"
-if os.get() == 'windows' then
-   print("skipping: python support not supported on Windows yet!")
-else
-    -- disable this for right now
-    --include "CpythonEngine"
-end
