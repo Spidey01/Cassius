1e8c04aaaefa7e90a29e549629a94dcdd9224c73
Terry M. Poulin <BigBoss1964@gmail.com>
2021-12-28 13:26:00 -0500

    The stupidest simple test for eval.

---
+++ Testing/lua_eval.cpp
@@ -0,0 +1,13 @@
+#include "./Common.hpp"
+
+#include <Cassius/Manager.hpp>
+#include <Cassius/Engine.hpp>
+#include <Cassius/SourceString.hpp>
+
+void run_test(const char* name)
+{
+    Cassius::Manager manager;
+    Cassius::Engine *engine = manager.CreateEngine(Cassius::LANG_LUA);
+    Cassius::SourceString code("2 + 2");
+    engine->Evaluate(code);
+}
